81332b012728cfbb3bdfb88e2f3f9731e47c6eff
kevinrocosta <kevinrodrigues04costa@gmail.com>
2025-07-21 15:25:12 -0300

    Add files via upload

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <p className='footer_text'>Learn More about the #SaveWarriorNun campaign at <a href="https://www.warriornun.com/">warriornun.com</a></p>
+        </div>
+    )
+}
+
+export default Footer;
